09b0b2abb1652657f0f312049b3385e5dc198dc2
Turashimye Chretien <130080990+chretie17@users.noreply.github.com>
2023-09-25 19:26:34 +0200

    newcrud.java

--- eclipse-workspace/new servlet/src/newcrud.java
+++ eclipse-workspace/new servlet/src/newcrud.java
@@ -11,25 +11,19 @@ public class newcrud extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Predefined username and password
         String validUsername = "chretien";
         String validPassword = "chretien";
 
-        // User-entered username and password
         String enteredUsername = request.getParameter("username");
         String enteredPassword = request.getParameter("password");
 
         if (validUsername.equals(enteredUsername) && validPassword.equals(enteredPassword)) {
-            // Authentication successful, redirect to a secured page
             response.sendRedirect("secured_page.jsp");
         } else {
-            // Authentication failed, check if it's a "Forget Password" request
             String forgetPassword = request.getParameter("forgetPassword");
             if ("true".equals(forgetPassword)) {
-                // Redirect to the "Forget Password" page (forgot.html)
                 response.sendRedirect("forgot.html");
             } else {
-                // Authentication failed, stay on the login page (login.html)
                 response.sendRedirect("login.html");
             }
         }
